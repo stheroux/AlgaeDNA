c6328aa5e3ce9aa5c78ae0f47f86f3d44cecc878
Susie Theroux <susanna.theroux@gmail.com>
2023-04-18 11:16:37 -0700

    Update qiime_params.sh

--- qiime_params.sh
+++ qiime_params.sh
@@ -4,6 +4,15 @@ DIR = ~/Documents/QIIME/
 # Location of fastq files
 SEQS1= ~/Documents/SEQS/
 
+# Cutadapt 
+# Copy and paste appropriate primers into cutadapt commands
+# P1 fwd primer
+# P2 rev primer
+# PE perc accept
+P1 = AGGTGAAGTAAAAGGTTCWTACTTAAA
+P2 = CCTTCTAATTTACCWACWACTG
+PE = 0.2 
+
 # Forward (L1) and Reverse (L2) trim lengths
 L1=240
 L2=240
@@ -11,4 +20,8 @@ L2=240
 # DNA reference database Fasta and Taxonomy files 
 FASTA= ~/Documents/DBS/SILVA_138/dna.cyano.fasta
 TAX= ~/Documents/DBS/SILVA_138/tax.cyano.txt
+
+# Blast 
+B1 = 0.97
+B2 = 1
 
